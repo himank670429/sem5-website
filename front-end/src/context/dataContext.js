9a8906e8d018d8dde2af1c7a6f5652a11842c605
himank singh <singhhimank241@gmail.com>
2023-07-06 14:07:17 +0530

    added client side optimisation

--- front-end/src/context/dataContext.js
+++ front-end/src/context/dataContext.js
@@ -2,13 +2,15 @@ import { useEffect, useState, createContext } from "react";
 export const DataContext = createContext();
 
 export default function DataProvider({children}){
-    const [data, setData] = useState([]);
+    const [data, setData] = useState(JSON.parse(localStorage.getItem('data')));
     useEffect(() => {
         fetch('https://sem5-api.onrender.com/data')
         .then(res => res.json())
         .then(data => {
             setData(data)
+            localStorage.setItem('data', JSON.stringify(data))
         })
+        .catch(err => console.log(err))
     }, [])
     return (    
     <DataContext.Provider value = {data}>
